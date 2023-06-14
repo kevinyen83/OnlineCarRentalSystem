e20ac19a56b4e4e7aa6fb7c4c554a7c92984cfbf
Kevin <kevinyenhaha@gmail.com>
2023-06-14 20:26:00 +1000

    v4.3 Updated UI and developed a new component for formPopup

--- clint/src/App.js
+++ clint/src/App.js
@@ -291,120 +291,123 @@ function App() {
                     removeItem={removeItem} 
                 />
 
-                {/* orderForm-popup */}
+                {/* form-popup */}
                 {showForm && (
-                    <div className="pop-form">
+                    <div className="popup-form">
                         <div className="overlay"></div>
-                            <div className="form-container">
-                                <div className="form-content">
-                                    <div className="cart-header">
-                                        <h2 className="form-header-title">Check Out</h2>
-                                        <button className="close-popup" onClick={checkout}>Close</button>
+                        <div className="form-container">
+                            <div className="form-header">
+                                <h2 className="form-header-title">ORDER FORM</h2>
+                                <div className="form-header-close-popup" onClick={checkout}>Close</div>
+                            </div>
+
+                            <div className="form-table-container">
+                                <form onSubmit={bookingCar}>
+                                    <div className="form-table-container-item">
+                                        <label htmlFor="firstName">First Name: </label>
+                                            <input
+                                                type="text"
+                                                name="firstName"
+                                                id="firstName"
+                                                value={firstName}
+                                                onChange={(event) => setFirstName(event.target.value)}
+                                            />
                                     </div>
-                                    <form className="order-form" onSubmit={bookingCar}>
-                                    <div className="form-table-container">
-                                        <div className="order-form-item">
-                                            <label htmlFor="firstName">First Name: </label>
-                                                <input
-                                                    type="text"
-                                                    name="firstName"
-                                                    id="firstName"
-                                                    value={firstName}
-                                                    onChange={(event) => setFirstName(event.target.value)}
-                                                />
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label htmlFor="name">Last Name: </label>
-                                                <input
-                                                    type="text"
-                                                    name="lastName"
-                                                    id="lastName"
-                                                    value={lastName}
-                                                    onChange={(event) => setLastName(event.target.value)}
-                                                />
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label>Email: </label>
-                                                <input
-                                                    type="text"
-                                                    name="email"
-                                                    id="email"
-                                                    value={email}
-                                                    onChange={(event) => setEmail(event.target.value)}          
-                                                />
-                                            <button type="button" onClick={validateEmail}>Validate Email</button>
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label>Address Line: </label>
-                                                <input
-                                                    type="text"
-                                                    name="address"
-                                                    id="address"
-                                                    value={address}
-                                                    onChange={(event) => setAddress(event.target.value)}
-                                                />
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label>City: </label>
-                                                <input
-                                                    type="text"
-                                                    name="city"
-                                                    id="city"
-                                                    value={city}
-                                                    onChange={(event) => setCity(event.target.value)}
-                                                />
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label>State: </label>
-                                            <select name="state" id="state" onChange={(event) => setState(event.target.value)}>
-                                                <option value="">-- Please select --</option>
-                                                <option value="ACT">Australian Capital Territory</option>
-                                                <option value="NSW">New South Wales</option>
-                                                <option value="NT">Northern Territory</option>
-                                                <option value="QLD">Queensland</option>
-                                                <option value="SA">South Australia</option>
-                                                <option value="TAS">Tasmania</option>
-                                                <option value="VIC">Victoria</option>
-                                                <option value="WA">Western Australia</option>
-                                            </select>
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label>Post Code: </label>
-                                                <input
-                                                    type="text"
-                                                    name="postCode"
-                                                    id="postCode"
-                                                    value={postCode}
-                                                    onChange={(event) => setPostCode(event.target.value)}
-                                                />
-                                        </div>
-
-                                        <div className="order-form-item">
-                                            <label>Payment Type: </label>
-                                            <select name="paymentType" id="paymentType" onChange={(event) => setPaymentType(event.target.value)}>
-                                                <optgroup label="paymentType">
-                                                    <option value="master">Master</option>
-                                                    <option value="visa">Visa</option>
-                                                </optgroup>
-                                            </select>   
-                                        </div>
+                                    <div className="form-table-container-item">
+                                        <label htmlFor="name">Last Name: </label>
+                                            <input
+                                                type="text"
+                                                name="lastName"
+                                                id="lastName"
+                                                value={lastName}
+                                                onChange={(event) => setLastName(event.target.value)}
+                                            />
+                                    </div>
+                                    <div className="form-table-container-item">
+                                        <label>Email: </label>
+                                            <input
+                                                type="text"
+                                                name="email"
+                                                id="email"
+                                                value={email}
+                                                onChange={(event) => setEmail(event.target.value)}          
+                                            />
+                                        <button type="button" onClick={validateEmail}>Validate Email</button>
+                                    </div>
+                                    <div className="form-table-container-item">
+                                        <label>Address Line: </label>
+                                            <input
+                                                type="text"
+                                                name="address"
+                                                id="address"
+                                                value={address}
+                                                onChange={(event) => setAddress(event.target.value)}
+                                            />
+                                    </div>
+                                    <div className="form-table-container-item">
+                                        <label>City: </label>
+                                            <input
+                                                type="text"
+                                                name="city"
+                                                id="city"
+                                                value={city}
+                                                onChange={(event) => setCity(event.target.value)}
+                                            />
+                                    </div>
+                                    <div className="form-table-container-item">
+                                        <label>State: </label>
+                                        <select name="state" id="state" onChange={(event) => setState(event.target.value)}>
+                                            <option value="">-- Please select --</option>
+                                            <option value="ACT">Australian Capital Territory</option>
+                                            <option value="NSW">New South Wales</option>
+                                            <option value="NT">Northern Territory</option>
+                                            <option value="QLD">Queensland</option>
+                                            <option value="SA">South Australia</option>
+                                            <option value="TAS">Tasmania</option>
+                                            <option value="VIC">Victoria</option>
+                                            <option value="WA">Western Australia</option>
+                                        </select>
+                                    </div>
+                                    <div className="form-table-container-item">
+                                        <label>Post Code: </label>
+                                            <input
+                                                type="text"
+                                                name="postCode"
+                                                id="postCode"
+                                                value={postCode}
+                                                onChange={(event) => setPostCode(event.target.value)}
+                                            />
+                                    </div>
+                                    <div className="form-table-container-item">
+                                        <label>Payment Type: </label>
+                                        <select name="paymentType" id="paymentType" onChange={(event) => setPaymentType(event.target.value)}>
+                                            <optgroup label="paymentType">
+                                                <option value="master">Master</option>
+                                                <option value="visa">Visa</option>
+                                            </optgroup>
+                                        </select>   
                                     </div>
                                     </form>
-                                <div className="order-form-footer">
-                                    <div className="order-form-price"><h3>Total Price: ${totalPrice.toFixed(2)}</h3></div>
-                                    <div className="order-form-price"><h3>Bond: ${bondValue}</h3></div>                        
-                                    <button type="button" onClick={checkout}>Continue Selection</button>
-                                    <button type="submit" onClick={bookingCar}>Booking</button>     
+                                </div>
+                            <div className="form-footer">
+                                <div className="form-footer-item">
+                                    <div className="form-footer-price"><h3>Total Price: ${totalPrice.toFixed(2)}</h3></div>
+                                </div>
+                                <div className="form-footer-item">
+                                    <div className="form-footer-bond"><h3>Bond: ${bondValue}</h3></div>                        
+                                </div>
+                                <div className="form-footer-item">
+                                    <div></div>
+                                </div>
+                                <div className="form-footer-item">
+                                    <div className="form-footer-btn" type="button" onClick={checkout}>Continue Selection</div>
+                                </div>
+                                <div className="form-footer-item">
+                                    <div className="form-footer-btn" type="submit" onClick={bookingCar}>Booking</div>     
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div>     
                 )}
             </div>
     </body>
